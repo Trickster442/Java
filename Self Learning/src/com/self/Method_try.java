de732988931535569d296aa5257e0af982c0f96e
Trickster <reone054@gmail.com>
2024-03-30 16:47:33 +0545

    arrays

--- Self Learning/src/com/self/Method_try.java
+++ Self Learning/src/com/self/Method_try.java
@@ -13,13 +13,29 @@ public class Method_try {
 		return z ;
 		
 		//why do we use static because to access without creating obj
+		// you can access method in two ways
+		// directly with static
+		// by creating obj 
+		// Class_name Obj_name = new Class_name();
+		// obj_name.obj();
+		
 	}
-	
+	static void change(int [] arr) {
+		arr[0] = 21 ;
+	}
 	public static void main(String[] args) {
 		int a = 5 ;
 		int b = 8 ;
 		int c = logic(a, b);
 		System.out.println(c);
+		
+		
+		//you can create array in this way
+		// int [] array_name = {1 , 3, 6 }
+		int [] i = {23, 32, 41, 50 };
+		change(i);
+		System.out.println(i[0]);
+		
 	}
 	
 }
