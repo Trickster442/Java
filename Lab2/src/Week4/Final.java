aa59e08d1695e939e45150ff71325e1a872c9a78
Trickster <reone054@gmail.com>
2024-04-09 22:25:16 +0545

    number guess finished

---
+++ Lab2/src/Week4/Final.java
@@ -0,0 +1,11 @@
+package Week4;
+
+public class Final {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Guess_game game1 = new Guess_game();
+		game1.guess();
+	}
+
+}
--- Lab2/src/Week4/Guess_game.java
+++ Lab2/src/Week4/Guess_game.java
@@ -2,29 +2,55 @@ package Week4;
 import java.util.Scanner;
 import java.util.Random;
 public class Guess_game {
-	public static void main(String [] args) {
-		for(int remaining = 10 ; remaining > 0 ; remaining--) {
-			Random correct = new Random();
-			int correct_num = correct.nextInt(10);
-			Scanner input = new Scanner(System.in);
-			System.out.print("The number you entered is : ");
-			int num = input.nextInt();
-			if (num != correct_num) {
-				System.out.println("Guess again");
-				System.out.printf("Number of guess remaining %d", --remaining);
-				System.out.println();
-				continue;
-			}
-			else if(num == correct_num){
-				System.out.println("Correct guess");
-				
-				break ;
-			}
-			else {
-				System.out.println("Finished");
-			}
-		
+//	public static void main(String [] args) {
+//		for(int remaining = 10 ; remaining > 0 ; remaining--) {
+//			Random correct = new Random();
+//			int correct_num = correct.nextInt(10);
+//			Scanner input = new Scanner(System.in);
+//			System.out.print("The number you entered is : ");
+//			int num = input.nextInt();
+//			if (num != correct_num) {
+//				System.out.println("Guess again");
+//				System.out.printf("Number of guess remaining %d", --remaining);
+//				System.out.println();
+//				continue;
+//			}
+//			else if(num == correct_num){
+//				System.out.println("Correct guess");
+//				
+//				break ;
+//			}
+//			else {
+//				System.out.println("Finished");
+//			}
+//		
+//		}
+//		
+//	}
+	int remaining;
+	Random correct = new Random();
+	final int correct_num = correct.nextInt(101);
+	
+	void guess(){
+		System.out.println("Select any number from 0 to 100");
+		for(remaining = 10 ; remaining > 0 ; remaining--) {
+		System.out.printf("\t Number of guess remaining %d \n", remaining);
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = input.nextInt();
+		if (num < correct_num) {
+			System.out.println("Low");
 		}
-		
+		else if(num > correct_num){
+			System.out.println("High");
+		}
+		else {
+			System.out.println("You won");
+			break;
+		}
 	}
+	if (remaining == 0) {
+           System.out.println("You lost. The correct number was: " + correct_num);
+    }	
+}
 }
