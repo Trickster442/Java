a39a024066d7233d7a8782a259f2e0538c4416e7
Trickster <reone054@gmail.com>
2024-04-16 10:47:29 +0545

    length of array

--- Self Learning/src/com/self/Array.java
+++ Self Learning/src/com/self/Array.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 public class Array {
 	public static void main(String[] arg) {
+		//Array is of fixed size so you need to declare the limit
+		// String [] days = new String[7];
 		String[] days = new String [] {"Monday", "Tuesday", "Wednesday"};
 		System.out.println(Arrays.toString(days));
+		System.out.println(days.length);
 	}
 }
+
