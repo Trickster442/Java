28bb0b9fe08eee87dea6582adea839eb4a1c24e7
Trickster <reone054@gmail.com>
2024-04-30 09:56:55 +0545

    user class

--- Lab2/src/Week3/QueryString.java
+++ Lab2/src/Week3/QueryString.java
@@ -1,5 +1,24 @@
 package Week3;
 
 public class QueryString {
-
+	String data ;
+	QueryString(String data){
+		this.data= data ;
+	}
+	
+	String getParameter(String name){
+		return null ;
+	}
+	
+	Boolean hasParameter(String Name) {
+		return false ;
+	}
+	
+	int getParameterOffset(String Name) {
+		return 0 ;
+	}
+	
+	static String decode (String s) {
+		return null ;
+	}
 }
