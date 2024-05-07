461186039be4b91153617cf00527327421d98bc7
Trickster <reone054@gmail.com>
2024-05-07 22:22:48 +0545

    j unit test

--- Lab2/src/Week3/VendingMachineTest.java
+++ Lab2/src/Week3/VendingMachineTest.java
@@ -3,42 +3,40 @@ import org.junit.Test;
 import static org.junit.Assert.* ;
 
 
-public class Lab3_Unittest {
+public class VendingMachineTest {
 
-	VendingMachine machine  = new VendingMachine();
 	@Test
 	public void testInitialBalance() {
+		VendingMachine machine  = new VendingMachine();
 		assertEquals(0, machine.getBalance()) ;
-	}
-	
+	} 
 	@Test
 	public void testInitialRevenue() {
+		VendingMachine machine  = new VendingMachine();
 		assertEquals(0 , machine.getRevenue());
 		}
-	
 	@Test 
 	public void testInsertCoins() {
+		VendingMachine machine  = new VendingMachine();
 		machine.insertCoin();
 		assertEquals(1, machine.getBalance());
 	}
-	
 	@Test
 	public void testRefund() {
+		VendingMachine machine  = new VendingMachine();
 		assertEquals(0, machine.refund());
 	}
-	
 	@Test
 	public void testVendFailure() {
+		VendingMachine machine  = new VendingMachine();
 		assertFalse(machine.vendCandyBar());
 	}
-	
 	@Test
 	public void testVendSuccess() {
+	VendingMachine machine  = new VendingMachine();
 	machine.insertCoin();
 	machine.insertCoin();
 	machine.insertCoin();
 	assertTrue(machine.vendCandyBar());
 	}
-	
-	
 }
