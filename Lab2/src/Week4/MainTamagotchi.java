bfd3b181fef853d8052f753493cd66bd7b19fd2d
Trickster <reone054@gmail.com>
2024-05-09 16:15:26 +0545

    tamagotchi

--- Lab2/src/Week4/MainTamagotchi.java
+++ Lab2/src/Week4/MainTamagotchi.java
@@ -2,17 +2,11 @@ package Week4;
 import java.util.Scanner;
 
 public  class MainTamagotchi{
-	
-	
 	public static void main(String[] args) {
 		Tamagotchi myTamagotchi = new Tamagotchi();
-		
 		Scanner scanner = new Scanner(System.in);
-		
-		
-	    System.out.println("Initial Status");
+		System.out.println("Initial Status");
 		displayStatus(myTamagotchi);
-	      
 	      while (true) {
 	            // Display menu options
 	            System.out.println("Choose an action:");
@@ -21,12 +15,11 @@ public  class MainTamagotchi{
 	            System.out.println("3. Clean the Tamagotchi");
 	            System.out.println("4. Pet the Tamagotchi");
 	            System.out.println("5. Pass time");
-	            System.out.println("6. Exit");
-
+	            System.out.println("6. Get current mood");
+	            System.out.println("7. Exit");
 	            // Read user input
 	            System.out.print("Choose number: ");
 	            int choice = scanner.nextInt();
-
 	            // Perform action based on user choice
 	            switch (choice) {
 	                case 1:
@@ -44,20 +37,20 @@ public  class MainTamagotchi{
 	                case 5:
 	                    myTamagotchi.passTime();
 	                    break;
-	                case 6:
+	                case 6: 
+	                	myTamagotchi.getMood();
+	                	break;
+	                case 7:
 	                    System.out.println("Exiting...");
 	                    scanner.close(); // Close the Scanner
 	                    return;
 	                default:
 	                    System.out.println("Invalid choice!");
 	            }
-
 	            // Display status after action
 	            displayStatus(myTamagotchi);
 	        }
 	    }
-	
-	
 	 private static void displayStatus(Tamagotchi tamagotchi) {
 	        System.out.println("Hungriness: " + tamagotchi.getHungriness());
 	        System.out.println("Happiness: " + tamagotchi.getHappiness());
