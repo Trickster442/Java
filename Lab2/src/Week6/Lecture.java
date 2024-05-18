2738d44a8b25d0d93ad7bbc1c1fbb3547262624f
Trickster <reone054@gmail.com>
2024-05-18 17:28:47 +0545

    index out of bound exception

--- Lab2/src/Week6/Lecture.java
+++ Lab2/src/Week6/Lecture.java
@@ -1,12 +1,18 @@
 package Week6;
+import java.util.ArrayList;
 
 public class Lecture {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] numArray = new int[4];
-		numArray[7] = 9 ;         //since array of length 4 is declared it will throw arrayoutofindex exception
+//		int[] numArray = new int[4];
+//		numArray[7] = 9 ;         //since array of length 4 is declared it will throw arrayoutofindex exception
 		
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("My");
+		list.add("Name");
+		System.out.println(list.get(2));
 	}
 
 }
